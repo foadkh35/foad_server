b3fe2fbc96ad3c9981aedb0e1fa1714b01382854
foadkh35 <foadkhatib5@gmail.com>
2024-06-25 18:35:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http")
-const app = require("./app");
+const app = require("./App");
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 app.listen(port)
